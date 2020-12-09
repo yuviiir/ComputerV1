38034d072be8cb17421a8b86f79df6f52a053712
Yuvir Sharma <yuvir@bbd.co.za>
2020-12-09 20:07:42 +0200

    did the solving algo

--- computerv1.py
+++ computerv1.py
@@ -1,11 +1,14 @@
 import sys
-import re
 
 def main():
     equation = simplify_equation(sys.argv[1].upper())
     print("Reduced form: " + equation + " = 0")
     degree = polynomial_degree(equation)
     print("Polynomial degree: " + str(degree))
+    if (degree > 2):
+        print("The polynomial degree is strictly greater than 2, I can't solve.")
+    else:
+        solve_equation(equation)
 
 def simplify_equation(equation):
     left = equation.split(" = ")[0]
@@ -64,5 +67,42 @@ def polynomial_degree(equation):
             highest_degree = degree if degree > highest_degree else highest_degree
         i += 1
     return (highest_degree)
+
+def solve_equation(equation) :
+    highest_power = polynomial_degree(equation)
+    equation_split = equation.split(" ")
+    i = 0
+    while (i < len(equation_split)):
+        if (equation_split[i] == "X^0"):
+            c = float(equation_split[i - 2]) if equation_split[0] != "-" else float(equation_split[i - 2]) * -1
+        if (equation_split[i] == "X^1"):
+            b = float(equation_split[i - 2]) if equation_split[i - 3] != "-" else float(equation_split[i - 2]) * -1
+        if (equation_split[i] == "X^2"):
+            a = float(equation_split[i - 2]) if equation_split[i - 3] != "-" else float(equation_split[i - 2]) * -1
+        i += 1
+    if highest_power == 0:
+        if c == 0 :
+            print ("The solution is all real numbers")
+        else :
+            print("No real solution")
+    if highest_power == 1:
+        solution = (c * -1) / (b)
+        print("Solution: " + str(solution))
+    if highest_power == 2:
+        if a == 0:
+            print("Can't solve!")
+            return(0)
+        if (b**2 - (4*a*c)) < 0:
+            print("Discriminant is strictly negative, the two solutions are:")
+            solution = (-b - (b**2 - (4*a*c))**0.5)/(2 * a)
+            print(str(solution))
+            solution = (-b + (b**2 - (4*a*c))**0.5)/(2 * a)
+            print(str(solution))
+            return(0)
+        print("Discriminant is strictly positive, the two solutions are:")
+        solution = (-b - (b**2 - (4*a*c))**0.5)/(2 * a)
+        print(str(solution))
+        solution = (-b + (b**2 - (4*a*c))**0.5)/(2 * a)
+        print(str(solution))
 
 main()
